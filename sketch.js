ed4c536487eccce785d525178b7191999777a7cb
Prof Andrea Ferreira <106248755+profandreaensina@users.noreply.github.com>
2022-05-30 14:28:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -78,29 +78,8 @@ function keyReleased() {
 
 function showBoats() {
   if (boats.length > 0) {
-    if (
-      boats[boats.length - 1] === undefined ||
-      boats[boats.length - 1].body.position.x < width - 300
-    ) {
-      var positions = [-40, -60, -70, -20];
-      var position = random(positions);
-      var boat = new Boat(width, height - 100, 170, 170, position);
-
-      boats.push(boat);
-    }
-
-    for (var i = 0; i < boats.length; i++) {
-      if (boats[i]) {
-        Matter.Body.setVelocity(boats[i].body, {
-          x: -0.9,
-          y: 0
-        });
-
-        boats[i].display();
-      } 
-    }
+    
   } else {
-    var boat = new Boat(width, height - 60, 170, 170, -60);
-    boats.push(boat);
+    
   }
 }
